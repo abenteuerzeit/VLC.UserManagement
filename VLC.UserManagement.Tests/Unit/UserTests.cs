e32cab7cc261e530751f7ea0816afb7032949fdd
abenteuerzeit <amroz.muzyka@gmail.com>
2023-02-25 03:12:22 +0100

    feat(testing): Add NUnit and Moq frameworks and unit test for CreateUser

---
+++ VLC.UserManagement.Tests/Unit/UserTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace VLC.UserManagement.Tests.Unit
+{
+    [TestFixture]
+    public class UserManagerTests
+    {
+        [Test]
+        public void CreateUser_ValidEmailAndPassword_ReturnsNewUser()
+        {
+            // Arrange
+            var mockUserRepository = new Mock<IUserRepository>();
+            var userManager = new UserManager(mockUserRepository.Object);
+            var email = new Email("john.doe@example.com");
+            var password = new Password("P@ssword123");
+
+            // Act
+            var user = userManager.CreateUser(email, password);
+
+            // Assert
+            Assert.IsNotNull(user);
+            Assert.AreEqual(email, user.Email);
+            Assert.IsTrue(user.Password.IsValid(password));
+        }
+    }
+}
