71b77a79ce4105c5c34ce00b28e6e79af5665e8d
abenteuerzeit <amroz.muzyka@gmail.com>
2023-02-25 06:53:10 +0100

    test(unit): Add CreateUser and UpdateUserEmail tests and TODOs

--- Tests/Unit/UserTests.cs
+++ Tests/Unit/UserTests.cs
@@ -2,6 +2,12 @@
 using Moq;
 using VLC.UserManagement.Entities;
 using VLC.UserManagement.ValueObjects;
+using Microsoft.AspNetCore.Http.HttpResults;
+using NUnit.Framework.Internal;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data;
+using System.Net;
+using System.Security.Principal;
 
 namespace VLC.UserManagement.Tests
 {
@@ -45,5 +51,54 @@ namespace VLC.UserManagement.Tests
             // Assert
             Assert.Throws<ArgumentException>(() => _userRepositoryMock.Setup(x => x.Add(newUser)).Returns(newUser));
         }
+
+        [Test]
+        public void CreateUser_WithWeakPassword_ShouldThrowException()
+        {
+            // Arrange
+            Email email = new("test@test.com");
+            Password weakPassword = new("password");
+
+            // Act
+            User newUser = new(email, weakPassword);
+            _userRepositoryMock.Setup(x => x.Add(newUser)).Returns(newUser);
+
+            // Assert
+            Assert.AreEqual(email, newUser.Email);
+            Assert.AreEqual(weakPassword, newUser.Password);
+        }
+
+        [Test]
+        public void UpdateUserEmail_WithValidEmail_ShouldReturnNewEmail()
+        {
+            // Arrange
+            Email email = new("test@test.com");
+            Password password = new("TestPassword1");
+            User newUser = new(email, password);
+            Email newEmail = new("newemail@test.com");
+
+            // Act
+            newUser.ChangeEmail(newEmail);
+            _userRepositoryMock.Setup(x => x.Update(newUser)).Returns(newUser);
+
+            // Assert
+            Assert.AreEqual(newEmail, newUser.Email);
+        }
+
+
+        // TODO:Test that a user cannot update their email address to an invalid email address.
+        // TODO:Test that a user can update their password.
+        // TODO:Test that a user cannot update their password to a weak password.
+        // TODO:Test that a user can change their name in their profile.
+        // TODO:Test that an admin can create a new user account and assign a role.
+        // TODO:Test that an admin can update a user's role to grant or revoke permissions.
+        // TODO:Test that an admin can view a list of all users and their roles.
+        // TODO:Test that an admin can deactivate a user's account to prevent them from accessing the app.
+        // TODO:Test that an admin can reactivate a previously deactivated user account.
+        // TODO:Test that a user can upload a profile picture to display on their profile.
+        // TODO:Test that an admin can reset a user's password if they forget it.
+        // TODO:Test that a user can reset their password using the email address associated with their account.
+        // TODO:Test that a user can delete their account and all associated data
     }
 }
+
